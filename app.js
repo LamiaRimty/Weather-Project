91043dbe4f11968d0729250c46848b45132ee94b
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-10-13 13:29:49 +0200

    get req with https module

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const https=require("https");
+
 
 app.get("/",function(req,res){
+
+    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=629f0c4e6dea1615eab7d478100e7a35&units=metric";
+    
+    https.get(url,function(response){  //callback func give us back response.
+    
+        console.log(response);
+    }); 
     res.send("Server is up and running");
 }); //user go to homepage
+
+
 
 app.listen(3000,function(){
  console.log("Server is running on port 3000 ");
